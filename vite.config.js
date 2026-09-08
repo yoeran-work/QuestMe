53defd44b2091cfa9b1aca14e37ddf8e41bc73ca
yoeran-work <meesteryoeran@gmail.com>
2026-09-08 09:00:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "QuestMe",
+        short_name: "QuestMe",
+        description: "Turn real life into an RPG.",
+        theme_color: "#080b14",
+        background_color: "#080b14",
+        display: "standalone",
+        start_url: "/QuestMe/",
+        scope: "/QuestMe/"
+      }
+    })
+  ],
+
+  base: "/QuestMe/"
+});
