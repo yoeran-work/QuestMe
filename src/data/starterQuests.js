99788562a4fa7878a48cfb5c7282430ecc2f8665
yoeran-work <meesteryoeran@gmail.com>
2026-09-11 09:24:13 +0200

    Update starterQuests.js

--- src/data/starterQuests.js
+++ src/data/starterQuests.js
@@ -3,31 +3,36 @@ const starterQuests = [
     id: 1,
     title: "2 liter water drinken",
     xp: 50,
-    y: 25
+    y: 25,
+    type: "daily"
   },
   {
     id: 2,
     title: "Mijn stappen halen",
     xp: 75,
-    y: 30
+    y: 30,
+    type: "daily"
   },
   {
     id: 3,
     title: "Haargroeispray gebruiken",
     xp: 15,
-    y: 5
+    y: 5,
+    type: "daily"
   },
   {
     id: 4,
     title: "Naar de sportschool",
     xp: 150,
-    y: 100
+    y: 100,
+    type: "daily"
   },
   {
     id: 5,
     title: "Kamer opruimen",
     xp: 300,
-    y: 250
+    y: 250,
+    type: "weekly"
   }
 ];
 
